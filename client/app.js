65f0fc27a4bab1a402b3961ae5327832df13214c
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-06-12 11:52:11 -0400

    Added basic angular framework to app.js; tweeked some of the CSS

--- client/app.js
+++ client/app.js
@@ -1,1 +1,14 @@
-// javascript goes here
+'use strict';
+
+angular.module('mainApp', [])
+       .crontroller('MainController', MainController);
+
+MainController.$inject = ['$scope'];
+
+function MainController($scope){
+  $scope.wordCount = wordCount;
+
+  function wordCount(t){
+
+  }
+}
