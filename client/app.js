e6f619098dfb215b62c9044002dbe4f102f96de8
bettyjobrotherton <elizabeth.j.brotherton@gmail.com>
2017-06-12 15:38:32 -0400

    Create app.js

--- client/app.js
+++ client/app.js
@@ -6,9 +6,41 @@ angular.module('mainApp', [])
 MainController.$inject = ['$scope'];
 
 function MainController($scope){
-  $scope.wordCount = wordCount;
+  $scope.startTest = startTest;
+  $scope.seconds = '00';
+  $scope.minutes = '00';
+  $scope.numberOfWords = 0;
+  var timerInterval;
 
-  function wordCount(t){
-
+  function startTest(){ //Starts the timer and begins the word count when triggered
+       startTimer();
+       countWords();
+  }
+       
+  function startTimer(){ //Starts the timer until the word count reaches the same as the sample text
+       if(!timerInterval){
+              timerInterval = setInterval(countUp, 1000);
+       }
+  } 
+  
+  function countUp(){
+       var secondsValue = parseInt($scope.seconds.text());
+       var minutesValue = parseInt($scope.minutes.text());
+       
+       if($scope.numberOfWords < 119){
+              if(secondsValue === 59) {
+                     seconds.text("00");
+                     minutes.text(pad(minutesValue + 1));
+              } else {
+                     seconds.text(pad(secondsValue + 1));
+                     minutes.text(pad(minutesValue));
+              }
+       } else {
+              clearInterval(timerInterval);
+              timerInterval = null;
+       }
+  }
+  
+  function countWords(){ //Counts the number of words in the text input box
   }
 }
